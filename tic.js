3e5d61b83e1c766e2a76eade687130ac85935b44
filosoferking <filosoferking@yahoo.com>
2015-07-28 17:50:07 -0700

    Merge Conflict Resolved

--- tic.js
+++ tic.js
@@ -3,19 +3,6 @@ var currentPlayer = playerX;
 var playerX;
 var playerO;
 
-<<<<<<< HEAD
-$(document).ready(function() {
-  var tile = "<div class='gameSquare'><img id='tile' src='img/x.png'></div>";
-  var row = "<div class='row'></div>";
-
-  for (var x = 1; x <= 3; x++) {
-
-    $(row).appendTo(".gameBoard");
-    console.log("Row");
-
-    for (var y = 1; y <= 3; y++) {
-
-=======
 $(document).ready(function(){
   console.log('document was loaded');
   var row = "<div class='col-sm-9 col-sm-offset-3 row'></div>";
@@ -27,85 +14,25 @@ $(document).ready(function(){
     $(row).appendTo(".gameBoard");
     
     for(var y = 1; y<=3; y++){
-    
->>>>>>> c11282fefb396d9271c1792fe89a1d72edef2bf6
+
       $(tile).appendTo(".row:nth-child(" + x + ")");
       console.log("Block");
       //" + x + " will be 1 or 2 or 3
     }
   }
 
-<<<<<<< HEAD
-
-var whoseTurnIsItAnyway = "<div class='whoseTurn'></div>";
-=======
   var whoseTurnIsItAnyway = "<div class='col-sm-9 col-sm-offset-3 whoseTurn'></div>";
->>>>>>> c11282fefb396d9271c1792fe89a1d72edef2bf6
+
 
   $("#playerContainer").append(whoseTurnIsItAnyway).text("Player X's move!").addClass("playerContainer");
 
-<<<<<<< HEAD
-=======
   $(".gameSquare").click(function(){
   var activeTile=$(".gameSquare")
   console.log('activeTile is',activeTile)
   playerMove(activeTile);
   });
->>>>>>> c11282fefb396d9271c1792fe89a1d72edef2bf6
 
 });
-
-<<<<<<< HEAD
-function playerMove() {
-  if($(this).text() == "") {
-    if(currentPlayer == playerX) {
-      $(this).text("X");
-      currentPlayer = playerO;
-    }
-    else {
-      $(this).text("O");
-      currentPlayer = playerX;
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-
 
 function playerMove(currentTile) {
   if($(this).text() == "") { //if the tile is empty
@@ -124,4 +51,4 @@ function playerMove(currentTile) {
       alert("Pick a different square."); //warning to let player know that square is taken
   };
 }
->>>>>>> c11282fefb396d9271c1792fe89a1d72edef2bf6
+

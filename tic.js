076a8f0e3517afdecc1cc5b31e74f4ed30aa62bf
filosoferking <filosoferking@yahoo.com>
2015-07-28 17:39:20 -0700

    Update

--- tic.js
+++ tic.js
@@ -1,35 +1,37 @@
 /********Global Variables*********/
-currentPlayer = playerX;
-playerX;
-playerO;
+var currentPlayer = playerX;
+var playerX;
+var playerO;
 
-$(document).ready(function(){
-var tile = "<div class='gameSquare'></div>";
-var row = "<div class='row'></div>";
-  
-  for(var x = 1; x<=3; x++){
+$(document).ready(function() {
+  var tile = "<div class='gameSquare'><img id='tile' src='img/x.png'></div>";
+  var row = "<div class='row'></div>";
 
-    $("#gameboard").append(row);
-    
-    for(var y = 1; y<=3; y++){
-    
+  for (var x = 1; x <= 3; x++) {
+
+    $(row).appendTo(".gameBoard");
+    console.log("Row");
+
+    for (var y = 1; y <= 3; y++) {
+
       $(tile).appendTo(".row:nth-child(" + x + ")");
+      console.log("Block");
       //" + x + " will be 1 or 2 or 3
     }
   }
 
-var whoseTurnIsItAnyway = "<div class="whoseTurn"></div>";
+
+var whoseTurnIsItAnyway = "<div class='whoseTurn'></div>";
 
 $("#gameboard").append(whoseTurnIsItAnyway);
 
-});
 
 $(".tile").click(function(){
   playerMove();
 });
 
 function playerMove() {
-  if($(this).text() == "" {
+  if($(this).text() == "") {
     if(currentPlayer == playerX) {
       $(this).text("X");
       currentPlayer = playerO;
@@ -40,4 +42,39 @@ function playerMove() {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
